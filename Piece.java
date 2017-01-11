effff3893279d27ec4717b1a8f4aae0dcd3a4058
gracecuenca <gcuenca@stuy.edu>
2017-01-11 16:32:32 -0500

    wrote the skeleton of the Piece.java file

---
+++ Piece.java
@@ -0,0 +1,52 @@
+public class Piece{
+
+
+    /*~~~~~~~~~~~~~INSTANCE VARIABLES~~~~~~~~~~~~~*/
+    protected int color; //-1 means not instantiated, 0 means black, 1 means white
+    protected int x; //column, -1 means not on board
+    protected int y; //row, -1 means not on board
+    protected int stackPos; //-1 is not on board, 0 is bottom of stack, 1 is second from bottom, etc.
+
+    /*~~~~~~~~~~~~~DEFAULT CONSTRUCTOR~~~~~~~~~~~~~*/
+    public Piece(){
+	color = -1;
+	x = -1;
+	y = -1;
+	stackPos = -1;
+    }
+
+    /*~~~~~~~~~~~~~OVERLOADED CONSTRUCTOR~~~~~~~~~~~~~*/
+    public Piece(int colores, int ex, int why){
+	color = colores;
+	x = ex;
+	y = why;
+	//will stackPos be determined by the Board? How will it be determined? For now, I set it as 0
+	stackPos = 0;
+    }
+    
+    /*~~~~~~~~~~~~~ACCESSORS~~~~~~~~~~~~~*/
+    public int getX(){
+	return x;
+    }
+
+    public int getY(){
+	return y;
+    }
+
+    public int getStack(){
+	return stackPos;
+    }
+
+    public int getColor(){
+	return color;
+    }
+
+    /*~~~~~~~~~~~~~MUTATORS~~~~~~~~~~~~~*/
+    public void setPos(int ex, int why){
+	x = ex;
+	y = why;
+    }
+
+    //some of the methods in the UML will not be applicable here anymore because we're implementing a Player.java class
+    //discuss and update the UML based on these changes
+}
