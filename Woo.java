3fdb1708119acc89f2490461889d8f9e2c5b8e0d
kingkwong <bkwong@stuy.edu>
2017-01-12 16:25:47 -0500

    skeleton driver file

---
+++ Woo.java
@@ -0,0 +1,46 @@
+import cs1.Keyboard;
+
+public class Woo {
+
+    /*~~~~~~~~~~~~~INSTANCE VARIABLES~~~~~~~~~~~~~*/
+    String player1;
+    String player2;
+    int oneNum; // number of stones player1 has
+    int oneCap; // number of capstones player1 has
+    int twoNum;
+    int twoCap;
+
+    /*~~~~~~~~~~~~~METHODS~~~~~~~~~~~~~*/
+    public static void playerTurn() {
+
+
+    }
+    public static void gameStart() {
+	oneNum = 21;
+	twoNum = 21;
+	oneCap = 1;
+	twoCap = 1;
+
+
+    }
+
+    public static boolean road() {
+	return true;
+    }
+
+    public boolean piecesLeft() {
+	return true;
+    }
+
+    public String printBoard() {
+	return "";
+    }
+
+    public String vertView(int x, int y) {
+	return "";
+    }
+
+
+
+
+}
